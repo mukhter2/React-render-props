01948fd98146dac7733d2211cad56d06b47a067e
Mukhter Hossain <mukhos1@gmail.com>
2023-02-12 12:25:12 +0600

    -updated from class to function

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-export default class ClickCounter extends React.Component {
-    render() {
-        const { counter, increment } = this.props;
-        return (
-            <div>
-                <button type="button" onClick={increment}>
-                    click {counter} times
-                </button>
-            </div>
-        );
-    }
+export default function ClickCounter({ counter, increment }) {
+    return (
+        <div>
+            <button type="button" onClick={increment}>
+                click {counter} times
+            </button>
+        </div>
+    );
 }
--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
-export default class HoverCounter extends React.Component {
-    render() {
-        const { counter, increment } = this.props;
-        return (
-            <div>
-                {/* eslint-disable-next-line jsx-a11y/mouse-events-have-key-events */}
-                <h1 onMouseOver={increment}>hovered {counter} times</h1>
-            </div>
-        );
-    }
+export default function HoverCounter({ counter, increment }) {
+    return (
+        <div>
+            {/* eslint-disable-next-line jsx-a11y/mouse-events-have-key-events */}
+            <h1 onMouseOver={increment}>hovered {counter} times</h1>
+        </div>
+    );
 }
