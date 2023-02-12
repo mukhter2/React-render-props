04ea2f60055c1bbe96606dcccfc2c5fd9116bbb8
Mukhter Hossain <mukhos1@gmail.com>
2023-02-12 11:26:56 +0600

    fod

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,13 +1,24 @@
-import withCounter from './HOC/withCounter';
+import React from 'react';
 
-function ClickCounter(props) {
-    const { counter, increment } = props;
-    return (
-        <div>
-            <button type="button" onClick={increment}>
-                click {counter} times
-            </button>
-        </div>
-    );
+export default class ClickCounter extends React.Component {
+    state = {
+        counter: 0,
+    };
+
+    increment = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1,
+        }));
+    };
+
+    render() {
+        const { counter } = this.state;
+        return (
+            <div>
+                <button type="button" onClick={this.increment}>
+                    click {counter} times
+                </button>
+            </div>
+        );
+    }
 }
-export default withCounter(ClickCounter);
--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,12 +1,23 @@
-import withCounter from './HOC/withCounter';
+import React from 'react';
 
-function HoverCounter(props) {
-    const { counter, increment } = props;
-    return (
-        <div>
-            {/* eslint-disable-next-line jsx-a11y/mouse-events-have-key-events */}
-            <h1 onMouseOver={increment}>hovered {counter} times</h1>
-        </div>
-    );
+export default class HoverCounter extends React.Component {
+    state = {
+        counter: 0,
+    };
+
+    increment = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + 1,
+        }));
+    };
+
+    render() {
+        const { counter } = this.state;
+        return (
+            <div>
+                {/* eslint-disable-next-line jsx-a11y/mouse-events-have-key-events */}
+                <h1 onMouseOver={this.increment}>hovered {counter} times</h1>
+            </div>
+        );
+    }
 }
-export default withCounter(HoverCounter);
